4e2d39e85820822c32c7f51611511c73808a95fd
JDLong <jdlong1980@gmail.com>
2018-12-20 21:26:50 -0500

    updated javascript file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,31 @@
-var
+// api bd3f1b468b3a4812a242ad3c5c1e25f0
+var searchTerm = "stock market crash";
+var beginDate = "20180101";
+var endDate = "20181220";
+var limit = 5;
+var page = 0;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "?begin_date=" + beginDate + "?end_date=" + endDate + "?page=" + page;
+
+$("#searchButton").on("click", function() {
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?=" + $("#searchTerm").value();
+
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
+    if (startYear.length === 4) {
+        queryURL += "?begin_date=" + startYear + "0101";
+    };
+    if (endYear.length === 4) {
+        queryURL += "?end_date=" + endYear + "0101";
+    };
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+    });
+});
+
+$("clearButton").on("click", function() {
+
+});
